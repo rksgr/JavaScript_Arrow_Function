565967424346c63aa302bedc30a0b13bdd2200f9
rksgr <r_kjha@yahoo.com>
2022-01-27 00:06:12 +0530

    No change

--- PinCodeValidation.js
+++ PinCodeValidation.js
@@ -45,3 +45,37 @@
     console.log("Result with whitespace in between: "+ result_with_whitespace);    // returns true
     console.log("Result without whitespace in between: " +result_without_whitespace);    // returns true
 }
+
+/**
+ * UC 1: Email Validation: Make sure at least three characters are present 
+ */
+{
+    let valid_email_id ="abc";
+    let invalid_email_id  = "ab";
+
+    // Atleast three characters should be present
+    let regex = RegExp("[a-zA-Z0-9]{3,}");
+
+    let result_valid_email_id = regex.test(valid_email_id);
+    let result_invalid_email_id = regex.test(invalid_email_id);
+
+    console.log("Result with valid email Id:"+ result_valid_email_id);      // Returns true
+    console.log("Result with invalid email Id:"+ result_invalid_email_id);  // Returns false
+}
+
+/**
+ * UC 2: Email validation: Make sure @ symbol and gmail after that is present
+ */
+{
+    let valid_email_id ="abc";
+    let invalid_email_id  = "ab";
+
+    // Atleast three characters should be present
+    let regex = RegExp("[a-zA-Z0-9]{3,}");
+
+    let result_valid_email_id = regex.test(valid_email_id);
+    let result_invalid_email_id = regex.test(invalid_email_id);
+
+    console.log("Result with valid email Id:"+ result_valid_email_id);      // Returns true
+    console.log("Result with invalid email Id:"+ result_invalid_email_id);  // Returns false
+}
