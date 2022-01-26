a333252e2c90e61bd9d4bda2fe0107378d560011
rksgr <r_kjha@yahoo.com>
2022-01-26 19:52:08 +0530

    Refactored: Restrict pin code from taking alphabets or special characters at beginning

--- PinCodeValidation.js
+++ PinCodeValidation.js
@@ -7,3 +7,14 @@
     let result = regex.test(pin);
     console.log(result);
 }
+/**
+ * UC 2: Restrict the pin code from taking alphabets or special characters at beginning
+ */
+{
+    let pin = "A400088";
+
+    // Beginning should contain only digits from 1 to 9
+    let regex = RegExp('^[1-9][0-9]{5}');
+    let result = regex.test(pin);
+    console.log(result);    // returns false
+}
