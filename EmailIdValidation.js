fa4bc16ae37ef01de0a366f737b0e80d6c2631ea
rksgr <r_kjha@yahoo.com>
2022-01-27 00:38:34 +0530

    Optional presence of certain special character(+-.-) before @ but alphanumeric characters must separate that from @ symbol

--- EmailIdValidation.js
+++ EmailIdValidation.js
@@ -54,3 +54,24 @@
     console.log("Is valid email Id1:"+ result_invalid_email_id1);  // Returns false
     console.log("Is valid email Id2: "+ result_invalid_email_id2);  // Returns false
 }
+
+/**
+ * UC 4: Email validation: There may be a certain special character(+-.-) before @ but alphanumeric characters must separate 
+ *                          it from @ symbol
+ */
+ {
+    let valid_email_id ="abc.xyz@gmail.com";
+    let invalid_email_id1  = "abc.@gmail.com";  // No alphanumeric character after dot
+    let invalid_email_id2 = "abc&@gmail.com";   // Ampersand not an allowed special character
+
+    // Atleast three characters should be present
+    let regex = RegExp("([a-zA-Z0-9]{3,}|[a-zA-Z0-9]{3,}[+_.-]?[a-zA-Z0-9]{1,})[@]{1}[g][m][a][i][l][.][c][o][m]");
+
+    let result_valid_email_id = regex.test(valid_email_id);
+    let result_invalid_email_id1 = regex.test(invalid_email_id1);
+    let result_invalid_email_id2 = regex.test(invalid_email_id2);
+
+    console.log("Is email Id Valid: "+ result_valid_email_id);      // Returns true
+    console.log("Is email Id1 valid: "+ result_invalid_email_id1);  // Returns false
+    console.log("Is email Id2 valid: "+ result_invalid_email_id2);  // Returns false
+}
