0dcdd022e93bed1dbf52d7db67955beb73fd17b8
rksgr <r_kjha@yahoo.com>
2022-01-27 00:14:31 +0530

    Make sure @ symbol and after that gmail is present in emailId

--- EmailIdValidation.js
+++ EmailIdValidation.js
@@ -14,3 +14,23 @@
     console.log("Result with valid email Id:"+ result_valid_email_id);      // Returns true
     console.log("Result with invalid email Id:"+ result_invalid_email_id);  // Returns false
 }
+
+/**
+ * UC 2: Email validation: Make sure @ symbol and gmail after that is present
+ */
+ {
+    let valid_email_id ="abc@gmail";
+    let invalid_email_id1  = "abcgoogle";
+    let invalid_email_id2 = "abc@xyz";
+
+    // Atleast three characters should be present
+    let regex = RegExp("[a-zA-Z0-9]{3,}[@]{1}[g][m][a][i][l]");
+
+    let result_valid_email_id = regex.test(valid_email_id);
+    let result_invalid_email_id1 = regex.test(invalid_email_id1);
+    let result_invalid_email_id2 = regex.test(invalid_email_id2);
+
+    console.log("Result with valid email Id:"+ result_valid_email_id);      // Returns true
+    console.log("Result with invalid email Id1:"+ result_invalid_email_id1);  // Returns false
+    console.log("Result with invalid email Id:2"+ result_invalid_email_id2);  // Returns false
+}
